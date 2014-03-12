a19d64930994d55cf6cd5350b06791676018348d
Michael Goldstein <michaelleegoldstein@gmail.com>
2014-03-12 00:50:54 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ruzure::Application.config.secret_key_base = '2b2f67b0b61383295f07acd466eca5c2c5bd7b3b6e68e5a8d5972072d9beef19d648711be4600be076a5cd54bf24979d05af7a15dec11953a1dcccb2c91c95c7'
