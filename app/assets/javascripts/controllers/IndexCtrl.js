cb33f790c9ddc0d6ff443b281806ede9671e7e8f
Michael Goldstein <michaelleegoldstein@gmail.com>
2014-03-17 00:10:26 -0500

    missed two files

---
+++ app/assets/javascripts/controllers/IndexCtrl.js
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: michael
+ * Date: 3/15/14
+ * Time: 11:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+app.controller('IndexController', ['$scope', function($scope){
+    $scope.title = 'app Stain!'
+}])
